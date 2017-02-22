3e390534a7c9cb4049604a4748f1db69cb6936ab
msmaltsev <maltsevmark@gmail.com>
2017-02-22 18:08:58 +0300

    added styles, textarea for search queries, switches for morphparser and quotes

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ import json, datetime
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run(port=getuid() + 1000)
+    
